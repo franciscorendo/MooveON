9e2eecce7a36d8c4e61da13cb8e7070c4911e77f
Camila <m.camiladepaul@gmail.com>
2019-11-15 11:39:48 -0300

    l
    l

--- MOVEON/js/generos.js
+++ MOVEON/js/generos.js
@@ -1,9 +1,7 @@
 window.onload = function(){
 
-
-
-// POPULARES
   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=a3f9467ae2c29b7ede89cca0ca14d893&language=en-US")
+
     .then(function(response){
       return  response.json();
     })
@@ -29,9 +27,6 @@ window.onload = function(){
     .catch(function(error){
       console.log(error)
     })
-
-
-
 
 
 
--- MOVEON/js/home.js
+++ MOVEON/js/home.js
@@ -87,9 +87,35 @@ fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=a3f9467ae2c29b7ede89cca
   console.log(error)
 })
 
+fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=a3f9467ae2c29b7ede89cca0ca14d893&language=en-US")
+.then(function(response){
+  return  response.json();
+})
+.then(function(data){
+  var array = data.results
+  var ul = document.querySelector()
+  console.log(array)
+  for (serie of array) {
+    var li = `
+   <a href="TUDETALLEDESERIE*">
+      <li>
+          <div class="uk-panel">
+              <img src="https://image.tmdb.org/t/p/original`+serie.poster_path+`" alt="">
+
+          </div>
+      </li>
+    </a>
+    `
+    ul.innerHTML += li;
+  }
+
+})
+.catch(function(error){
+  console.log(error)
+})
 
 
-
+// <li><a href="series.html?idGenre=00000&nameGenre=action">Action</a></li>
 
 
 }
