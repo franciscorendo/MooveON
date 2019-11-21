e961c90e2d475a66f6ea5bfe7fc54906ef2ae9b7
finiqui <jquirno@udesa.edu.ar>
2019-11-21 20:39:06 -0300

    resultado y busqueda maso

--- MOVEON/js/resultadoavanzado.js
+++ MOVEON/js/resultadoavanzado.js
@@ -10,28 +10,31 @@ window.addEventListener('load',function(){
   var anio = search.get('anioElegido')
   console.log(anio)
 
-  fetch("https://api.themoviedb.org/3/discover/tv?api_key=a3f9467ae2c29b7ede89cca0ca14d893&language=en-US&sort_by=popularity.desc&first_air_date_year=2019&page=1&with_genres=18&without_genres=")
+
+  fetch("https://api.themoviedb.org/3/discover/tv?api_key=a3f9467ae2c29b7ede89cca0ca14d893&language=en-US&sort_by="+orden+"&first_air_date_year="+ anio + "&page=1&with_genres="+incluir+"&without_genres="+excluir)
     .then(function(respuesta){
       return respuesta.json();
   })
   .then(function(datos){
-    console.log(datos);
+    // console.log(datos);
 
-var arrayDeSeries = datos.results
-//     var ul = document.querySelector('div.ul')
-//     var li = ""
-//     //parte fija img
-//    var urlIMG = "https://image.tmdb.org/t/p/original/"
-//    //recorro el array
-// for (var i=0; i < arrayDeSeries.length; i++){
-//   if (arrayDeSeries[i].poster_path != null){
-//     li = "<li>"
-//       li += "<a href='detalle.html?idPelicula="+arrayDeSeries[i].id+"'>"
-//       li += "<p>" + arrayDeSeries[i].title + "</p>"
-//       li += "<img src = '" + urlImg + arrayDeSeries[i].poster_path + "' style='width:300px;'>"
-//       li += "</a>"
-//       li += "</li>"
-//       ul.innerHTML += li
-//  }
+    var arrayDeSeries = datos.results
+    console.log(arrayDeSeries);
+    var ul = document.querySelector('.resultados')
+    var li = ""
+     //parte fija img
+     var urlIMG = "https://image.tmdb.org/t/p/original/"
+    //recorro el array
+ for (var i=0; i < arrayDeSeries.length; i++){
+
+      li = "<li>"
+       li += "<a href='detalle.html?idPelicula="+arrayDeSeries[i].id+"'>"
+       li += "<p>" + arrayDeSeries[i].original_name + "</p>"
+       li += "<img src = '" + urlIMG + arrayDeSeries[i].poster_path + "' style='width:300px;'>"
+       li += "</a>"
+       li += "</li>"
+       ul.innerHTML += li;
+
+ }
 })
 })
